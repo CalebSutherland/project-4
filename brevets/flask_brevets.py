af2ceb72cbd47cfdf655bd8bce7128d2c182a405
csutherl <sutherlandcaleb12@gmail.com>
2023-02-26 17:24:49 -0800

    implemented ACP Brevet Control Times Calculator logic

--- brevets/flask_brevets.py
+++ brevets/flask_brevets.py
@@ -12,6 +12,7 @@ import config
 
 import logging
 
+
 ###
 # Globals
 ###
@@ -51,14 +52,18 @@ def _calc_times():
     """
     app.logger.debug("Got a JSON request")
     km = request.args.get('km', 999, type=float)
+    brevet_dist = request.args.get('brevet_dist', 200, type=float)
+    start_time = request.args.get('start_time', type=str)
+    start = arrow.get(start_time, "YYYY-MM-DDTHH:mm")
     app.logger.debug("km={}".format(km))
     app.logger.debug("request.args: {}".format(request.args))
+    app.logger.debug("start={}".format(start))
     # FIXME!
     # Right now, only the current time is passed as the start time
     # and control distance is fixed to 200
     # You should get these from the webpage!
-    open_time = acp_times.open_time(km, 200, arrow.now().isoformat).format('YYYY-MM-DDTHH:mm')
-    close_time = acp_times.close_time(km, 200, arrow.now().isoformat).format('YYYY-MM-DDTHH:mm')
+    open_time = acp_times.open_time(km, brevet_dist, start).format('YYYY-MM-DDTHH:mm')
+    close_time = acp_times.close_time(km, brevet_dist, start).format('YYYY-MM-DDTHH:mm')
     result = {"open": open_time, "close": close_time}
     return flask.jsonify(result=result)
 
